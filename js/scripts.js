18d4705c29728683afed9022442c9a0024486d03
zhuraavl <zhuraavl@gmail.com>
2017-12-22 00:12:48 +0200

    init

---
+++ js/scripts.js
@@ -0,0 +1,283 @@
+$(document).ready(function(){
+   setTimeout(function(){
+    $('body').addClass("load");
+   }, 700);
+  
+  $(".header-link a, .top-logo-box a, .lets-talk a, .service-links a").click(function(e){
+    e.preventDefault();
+    $('body').removeClass("load");
+    var url = this.href;
+    setTimeout(function(){
+        location.href = url;
+    }, 700);
+});
+
+
+});
+
+
+
+
+var hero = $('.fixed-hero');
+var range = 300;
+
+$(window).on('scroll', function () {
+  
+  var scrollTop = $(this).scrollTop(),
+      height = hero.outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+
+  hero.css({ 'opacity': calc });
+
+  if (calc > '1') {
+    hero.css({ 'opacity': 1 });
+  } else if ( calc < '0' ) {
+    hero.css({ 'opacity': 0 });
+  }
+  
+});
+
+
+
+
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 850) {
+    $('footer').addClass("show");
+  } else {
+    $('footer').removeClass("show");
+  }
+});
+
+
+$(window).scroll(function() {
+   $(".page-bottom").removeClass("bottom");
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       //you are at bottom
+       $(".page-bottom").addClass("bottom");
+   }
+});
+
+
+
+
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+  // Basic options for a simple Google Map
+  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+  var mapOptions = {
+    // How zoomed in you want the map to start at (always required)
+    zoom: 15,
+    disableDefaultUI: true,
+    // The latitude and longitude to center the map (always required)
+    center: new google.maps.LatLng(50.421861, 30.553389),
+
+    // How you would like to style the map. 
+    // This is where you would paste any style found on Snazzy Maps.
+    styles: [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": "-51"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "gamma": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            },
+            {
+                "gamma": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            },
+            {
+                "gamma": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+]
+  };
+
+  // Get the HTML DOM element that will contain your map 
+  // We are using a div with id="map" seen below in the <body>
+  var mapElement = document.getElementById('map');
+
+  // Create the Google Map using our element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
+
+  // Let's also add a marker while we're at it
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(50.421861, 30.553389),
+    map: map,
+    title: 'Snazzy!'
+  });
+}
