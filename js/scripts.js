e43164e0fa1b43df4ceced218526d3d11e742c97
Yuriy Gruda <grumarket@gmail.com>
2018-01-14 23:46:07 +0200

    upd rus form

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+  
+  $("#contact-form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+  
+  
+  
+  
    setTimeout(function(){
     $('body').addClass("load");
    }, 700);
@@ -281,3 +301,24 @@ function init() {
     title: 'Snazzy!'
   });
 }
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
