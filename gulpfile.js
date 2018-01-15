dccc0b43c5a9210af05b7a413b8275445ab61c10
Yuriy Gruda <grumarket@gmail.com>
2018-01-15 02:59:46 +0200

    upd ua form

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp      = require('gulp'), // Подключаем Gulp
+    sass        = require('gulp-sass'), //Подключаем Sass пакет,
+    jade = require('gulp-jade'),
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
+
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('dev/sass/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('jade', function() {
+    return gulp.src('dev/jade/**/*.jade')
+        .pipe(jade()) 
+        .pipe(gulp.dest('./'))
+				.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: './' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass', 'jade'], function() {
+    gulp.watch('dev/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch('dev/jade/*.jade', ['jade']);
+});
